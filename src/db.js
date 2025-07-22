376475ea8bb20f231cb91a5b0d742ed28a1cdd24
wilton <faltdor@gmail.com>
2025-07-21 23:07:35 -0500

    add ssl

--- src/db.js
+++ src/db.js
@@ -3,9 +3,9 @@ require("dotenv").config();
 
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
-   /* ssl: {
+    ssl: {
         rejectUnauthorized: false
-    }*/
+    }
 });
 
 module.exports = pool;
